7dd9b980489113bba70ba33304937bc84fa89901
Ahmed Nabil <bplahmed28d@gmail.com>
2024-08-12 14:18:53 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// 1. Multiply two numbers to get a result of 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // multiply equals 62
 
-// Write your code here
+// 2. Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // random is a positive integer
+
+// 3. Calculate the remainder of dividing two numbers to get a result of 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // mod equals 4
+
+// 4. Find the highest number in a set
+const max = Math.max(1, 5, 10, 20); // max equals 20
